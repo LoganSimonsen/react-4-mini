83cdf065a99cf10c7784ec222cf6c8f022640c25
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-02-06 12:19:54 -0600

    completed

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { HashRouter as Router } from 'react-router-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
+
